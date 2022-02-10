e50e2cf4febe36448c38901aaac9d09975ac7c0b
Mateusz Zając <matzayonc@gmail.com>
2022-02-10 15:34:05 +0100

    almost from implementation

--- decimal_core/src/lib.rs
+++ decimal_core/src/lib.rs
@@ -20,27 +20,61 @@ pub fn decimal(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let underlying_type = first_field.ty.to_token_stream();
 
+    let field_name = match first_field.ident.clone() {
+        Some(ident) => quote! {#ident},
+        None => quote! {0},
+    };
+
     let struct_name = decimal_struct.ident;
 
-    let scale_definition = quote!(pub const SCALE: u8 = #parsed_scale;);
     let get_scale_definition = quote!(
 
-        impl #struct_name {
+        impl Decimal<#underlying_type> for #struct_name {
             fn get_scale(&self) -> u8{
-                SCALE
+                #parsed_scale
             }
 
             fn get_value(&self) -> #underlying_type {
-                self.0
+                self.#field_name
+            }
+
+
+        }
+
+        // this should be an impl of From but getting errors :(
+        impl #struct_name {
+            fn here<T: TryFrom<#underlying_type>>(&self) -> T {
+                match T::try_from(self.#field_name) {
+                    Ok(v) => v,
+                    Err(_) => panic!("could not parse {} to {}", "T", "u8"),
+                }
             }
         }
 
+    );
 
+    println!(
+        "CCC {}",
+        quote!(
+                    // this should be an impl of Into but getting errors :(
+                        impl #struct_name {
+                            fn here<T: TryFrom<#underlying_type>(&self) -> T {
+
+                                universal_into::<T, #underlying_type>(self.#field_name)
+
+                                // match self.#field_name.try_into() {
+                                //     Ok(v) => v,
+                                //     Err(_) => panic!("could not parse {} to {}", "T", "u8"),
+                                // }
+                            }
+                        }
+        )
+        .to_token_stream()
+        .to_string()
     );
 
     let mut result = item.clone();
     result.extend(proc_macro::TokenStream::from(quote! {
-        #scale_definition
         #get_scale_definition
     }));
 
--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,55 @@ pub use crate::uint::U256;
 
 use decimal_core::decimal;
 
+use std::convert::TryInto;
+
+trait Decimal<T> {
+    fn get_scale(&self) -> u8;
+    fn get_value(&self) -> T;
+}
+
+fn universal_into<Y, T: TryInto<Y>>(a: T) -> Y {
+    match a.try_into() {
+        Ok(v) => v,
+        Err(_) => panic!("could not parse {} to {}", "T", "u8"),
+    }
+}
+
 #[decimal(12)]
-struct D(u8);
+struct N(u8);
+
+#[decimal(12)]
+struct K {
+    v: u8,
+}
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
 
     #[test]
+    pub fn tr() {
+        assert_eq!(universal_into::<u8, u16>(255), 255);
+        assert_eq!(universal_into::<u8, u8>(255), 255);
+    }
+
+    #[test]
+    pub fn test_from() {
+        let n = N(0);
+        let k: u8 = n.here();
+        assert_eq!(k, 0)
+    }
+
+    #[test]
     pub fn flow() {
-        let d = D(42);
+        let d = N(42);
 
         assert_eq!(d.get_scale(), 12);
         assert_eq!(d.get_value(), 42);
+
+        let t = K { v: 42 };
+
+        assert_eq!(t.get_scale(), 12);
+        assert_eq!(t.get_value(), 42);
     }
 }
