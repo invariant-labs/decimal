08cd2fae3686b20a5ddd1cffbb123dc580ced23b
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-05-09 20:21:39 +0200

    Add max_value base function

--- decimal_core/src/base.rs
+++ decimal_core/src/base.rs
@@ -28,6 +28,10 @@ pub fn generate_base(characteristics: DecimalCharacteristics) -> proc_macro::Tok
                 created
             }
 
+            fn max_value() -> Self::U {
+                Self::U::MAX
+            }
+
             fn here<T: TryFrom<Self::U>>(&self) -> T {
                 match T::try_from(self.#field_name) {
                     Ok(v) => v,
